fc05abd2e0045dcc4c15d053db7b11c28ba9bccc
bumax1989 <bumax1989@gmail.com>
2021-03-27 09:32:43 +0300

    add MathOperations.java

---
+++ src/test/java/homework/lectures/lecture1/MathOperations.java
@@ -0,0 +1,22 @@
+package homework.lectures.lecture1;
+
+public class MathOperations {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 7;
+        int result;
+        double divideResult;
+
+        result = a+b;
+        System.out.println("Резузльтат сложения a+b: " + result);
+
+        result = a-b;
+        System.out.println("Резузльтат вычитания a-b: " + result);
+
+        result = a*b;
+        System.out.println("Резузльтат умножения a*b: " + result);
+
+        divideResult= (double) a / (double) b;
+        System.out.println("Резузльтат деления a/b: " + divideResult);
+    }
+}
