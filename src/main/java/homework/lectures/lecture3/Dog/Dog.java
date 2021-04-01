c5b0572dd756d3d02aab87ea0e39f5cf3fa031f1
bumax1989 <bumax1989@gmail.com>
2021-04-01 23:43:17 +0300

    task with Dog class has been done

---
+++ src/main/java/homework/lectures/lecture3/Dog/Dog.java
@@ -0,0 +1,26 @@
+package homework.lectures.lecture3.Dog;
+
+public class Dog {
+    private int age;
+    private String name;
+    private String color;
+
+    public Dog(int age, String name, String color) {
+        this.age = age;
+        this.name = name;
+        this.color = color;
+    }
+
+    public void voice() {
+        System.out.println(name + " is barking. He is " + age + " years old.");
+    }
+
+    public void eat() {
+        System.out.println(name + " is eating. His color is " + color);
+    }
+
+    public void sleep() {
+        System.out.println(name + " is sleeping");
+    }
+
+}
---
+++ src/main/java/homework/lectures/lecture3/Dog/DogDemo.java
@@ -0,0 +1,12 @@
+package homework.lectures.lecture3.Dog;
+
+public class DogDemo {
+    public static void main(String[] args) {
+        Dog dog = new Dog(5,"Bobik", "white");
+
+        dog.eat();
+        dog.sleep();
+        dog.voice();
+
+    }
+}
