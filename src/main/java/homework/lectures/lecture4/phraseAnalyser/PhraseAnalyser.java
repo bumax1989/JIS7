e331ba1c73c1f9a3e43623c9202490026f63dfa4
bumax1989 <bumax1989@gmail.com>
2021-04-02 16:04:50 +0300

    Pharse analyser has been done

---
+++ src/main/java/homework/lectures/lecture4/phraseAnalyser/PhraseAnalyser.java
@@ -0,0 +1,29 @@
+package homework.lectures.lecture4.phraseAnalyser;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class PhraseAnalyser {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите строку");
+        String readLine = reader.readLine();
+
+        System.out.println(analyse(readLine));
+    }
+
+    public static String analyse(String text) {
+
+        String answer;
+
+        if ((text.startsWith("Make")) && (text.endsWith("great again"))) {
+            answer = "It stands no chance";
+        } else if ((text.startsWith("Make")) || (text.endsWith("great again"))) {
+            answer = "It could be worse";
+        } else
+            answer = "It is fine, really";
+
+        return answer;
+
+    }
+}
